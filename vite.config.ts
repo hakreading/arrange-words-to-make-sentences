668d20d59f224677c5e9bff71999a2c74036f09c
hakreading <bon2beaking5@gmail.com>
2026-09-11 11:37:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/arrange-words-to-make-sentences/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
